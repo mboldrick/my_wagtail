bf135ff9f5d1cc4c6958c58550db28ac8fe2e3fd
Mike Boldrick <mb@boldrick.net>
2025-04-27 11:29:37 -0700

    Initial commit

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wa#$b$_8d8xrtxgp7p%n(#kedg)mnbn%)&tqcb$23(tg2_8(t%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
